97f9a89a333a6b4869055270bb7e12dfab40289d
Leonardo Xavier <leonardoxw@gmail.com>
2022-08-21 17:01:18 -0300

    Custom exception handling

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -8,13 +8,20 @@ namespace chess_console
     {
         static void Main(string[] args)
         {
-            Board B = new Board(8, 8);
+            try
+            {
+                Board B = new Board(8, 8);
 
-            B.putPiece(new Rock(Color.Black, B), new Position(0, 0));
-            B.putPiece(new Rock(Color.Black, B), new Position(1, 3));
-            B.putPiece(new King(Color.Black, B), new Position(2, 4));
+                B.putPiece(new Rock(Color.Black, B), new Position(0, 0));
+                B.putPiece(new Rock(Color.Black, B), new Position(1, 3));
+                B.putPiece(new King(Color.Black, B), new Position(0, 1));
 
-            Screen.boardPrinter(B);
+                Screen.boardPrinter(B);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- chess-console/board/Board.cs
+++ chess-console/board/Board.cs
@@ -18,11 +18,43 @@
             return pieces[line, column];
         }
 
+        public Piece piece(Position pos)
+        {
+            return pieces[pos.line, pos.column];
+        }
+
+        public bool isPiece(Position pos)
+        {
+            positionValidation(pos);
+            return piece(pos) != null;
+        }
+
         public void putPiece(Piece p, Position pos)
         {
+            if (isPiece(pos))
+            {
+                throw new BoardException("There is already a piece in that position!");
+            }
             pieces[pos.line, pos.column] = p;
             p.position = pos;
 
+        }
+
+        public bool validPosition(Position pos)
+        {
+            if(pos.line < 0|| pos.line >= lines || pos.column < 0 || pos.column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void positionValidation(Position pos)
+        {
+            if(!validPosition(pos))
+            {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
